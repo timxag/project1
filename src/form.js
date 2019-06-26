2da74922f1cb5965e9d263111c9af1c674bf4344
timxag <timxag@yandex.ru>
2019-06-26 17:38:30 +0300

    js autocomplete beta version

--- src/form.js
+++ src/form.js
@@ -1,112 +1,156 @@
-import React from 'react';
-import './index.css'
-import { FormErrors } from './FormErrors'
+import React from "react";
+import "./index.css";
+import { FormErrors } from "./FormErrors";
+
 var linkColor;
 var titleColor;
+var counter = 1;
+
 export default class Form extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            link: '',
-            title: '',
-            formErrors: { link: '', title: '' },
-            linkValid: false,
-            titleValid: false,
-            formValid: false,
+  constructor(props) {
+    super(props);
+    this.linkChange = this.linkChange.bind(this);
+    this.autoComplete = this.autoComplete.bind(this);
+    this.linkRef = React.createRef();
+    this.show = this.show.bind(this);
+    this.hide = this.hide.bind(this);
+    this.state = {
+      link: "",
+      title: "",
+      formErrors: { link: "", title: "" },
+      linkValid: false,
+      titleValid: false,
+      formValid: false,
+      data: this.props.arr
+    };
+  }
+  autoComplete(Arr, Input) {
+    return Arr.filter(e => e.includes(Input));
+  }
+  handleUserInput = ({ target: { name, value } }) => {
+    this.validateField(name, value);
+    let newData = this.autoComplete(this.props.arr, value);
+    this.setState({ data: newData, [name]: value });
+    console.log({ newData });
+  };
 
-        }
+  validateField(fieldName, value) {
+    let fieldValidationErrors = this.state.formErrors;
+    let linkValid = this.state.linkValid;
+    let titleValid = this.state.titleValid;
+    switch (fieldName) {
+      case "link":
+        linkValid = value.match(
+          /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?$/i
+        );
+        fieldValidationErrors.link = linkValid ? "" : " is invalid";
+        linkColor = linkValid ? "green" : "red";
+
+        break;
+      case "title":
+        titleValid = value.length >= 3;
+        fieldValidationErrors.title = titleValid ? "" : " is too short";
+        titleColor = titleValid ? "green" : "red";
+
+        break;
+      default:
+        break;
     }
+    console.log(linkValid);
 
-    handleUserInput = (e) => {
-        const name = e.target.name;
-        const value = e.target.value;
-        this.setState({ [name]: value },
-            () => { this.validateField(name, value) });
-
+    this.setState(
+      {
+        formErrors: fieldValidationErrors,
+        linkValid: linkValid,
+        titleValid: titleValid
+      },
+      this.validateForm
+    );
+  }
+  validateForm() {
+    this.setState({
+      formValid: this.state.linkValid && this.state.titleValid
+    });
+  }
+  errorClass(error) {
+    return error.length === 0 ? "" : "has-error";
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    {
+      this.props.onSubmit(this.state.link, this.state.title);
     }
+  }
+  show() {
+    this.linkRef.current.classList.remove("dropdown_hid");
+    this.linkRef.current.classList.add("dropdown1");
+    console.log("show: ", this.linkRef);
+  } //
+  hide() {
+    this.linkRef.current.classList.remove("dropdown1");
+    this.linkRef.current.classList.add("dropdown_hid");
+    console.log("hide:", this.linkRef);
+  }
+  linkChange(url) {
+    this.setState({ link: url });
+  }
+  render() {
+    //console.log(this.linkRef);
 
-    validateField(fieldName, value) {
-        let fieldValidationErrors = this.state.formErrors;
-        let linkValid = this.state.linkValid;
-        let titleValid = this.state.titleValid;
-        switch (fieldName) {
-            case 'link':
-                linkValid = value.match(/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?$/i);
-                fieldValidationErrors.link = linkValid ? '' : ' is invalid';
-                linkColor = linkValid ? "green" : "red"
+    return (
+      <>
+        <form className="inputform" onSubmit={this.handleSubmit.bind(this)}>
+          <div className="requirements">
+            <FormErrors formErrors={this.state.formErrors} />
+          </div>
+          <div class="__inputWrapper">
+            <label htmlFor="link">Image URL</label>
+            <input
+              id="form"
+              type="link"
+              className="form-control"
+              autocomplete="off"
+              style={{ borderColor: linkColor }}
+              onFocus={this.show}
+              onBlur={this.hide}
+              name="link"
+              value={this.state.link}
+              onChange={this.handleUserInput}
+            />
+            <br />
+            <ul className="dropdown_hid" ref={this.linkRef}>
+              {this.state.data.map(url => (
+                <li tabIndex={counter++} onClick={this.handleUserInput}>
+                  {url}
+                </li>
+              ))}
+            </ul>
+          </div>
 
-                break;
-            case 'title':
-                titleValid = value.length >= 3;
-                fieldValidationErrors.title = titleValid ? '' : ' is too short';
-                titleColor = titleValid ? "green" : "red"
-
-                break;
-            default:
-                break;
-        }
-        console.log(linkValid);
-
-        this.setState({
-            formErrors: fieldValidationErrors,
-            linkValid: linkValid,
-            titleValid: titleValid,
-        }, this.validateForm);
-
-    }
-    validateForm() {
-        this.setState({
-            formValid: this.state.linkValid &&
-                this.state.titleValid
-        });
-    }
-    errorClass(error) {
-        return (error.length === 0 ? '' : 'has-error');
-    }
-    handleSubmit(e) {
-        e.preventDefault();
-        {
-            this.props.onSubmit(this.state.link, this.state.title)
-        }
-    }
-    autoComplete(Arr, Input) {
-        return Arr.filter(e => e.toLowerCase().includes(Input.toLowerCase()));
-    }
-    render() {
-
-        return (
-            <form className="inputform" onSubmit={this.handleSubmit.bind(this)} >
-                <div className="requirements">
-                    <FormErrors formErrors={this.state.formErrors} />
-
-                </div>
-                <div className={
-                    `form-group
-                    ${this.errorClass(this.state.formErrors.title)}`
-                }>
-                    <label htmlFor="link">Image URL</label>
-                    <input type="link" className="form-control" autocomplete="off" list="cc"
-                        style={{ borderColor: linkColor }}
-                        name="link" value={this.state.link} onChange={this.handleUserInput} />
-                </div>
-                <br></br>
-                <div className={`form-group
-                 ${this.errorClass(this.state.formErrors.title)}`}>
-                    <label htmlFor="title">Description</label>
-                    <input type="title" className="form-control" autocomplete="off"
-                        style={{ borderColor: titleColor }}
-                        name="title" value={this.state.title} onChange={this.handleUserInput} />
-                </div>
-                <button type="submit" className="btn btn-primary"
-                    disabled={!this.state.formValid}>Add</button>
-                <datalist id="cc">
-                    {this.props.arr.map(
-                        arr => (
-                            <option value={arr}></option>
-                        )
-                    )}
-                </datalist>
-            </ form >
-        )
-    }
+          <div
+            className={`form-group
+                 ${this.errorClass(this.state.formErrors.title)}`}
+          >
+            <label htmlFor="title">Description</label>
+            <input
+              type="title"
+              className="form-control"
+              autocomplete="off"
+              style={{ borderColor: titleColor }}
+              name="title"
+              value={this.state.title}
+              onChange={this.handleUserInput}
+            />
+          </div>
+          <button
+            type="submit"
+            className="btn btn-primary"
+            disabled={!this.state.formValid}
+          >
+            Add
+          </button>
+        </form>
+      </>
+    );
+  }
 }
